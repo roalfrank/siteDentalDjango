af21b0de9d3324a17ae9bd8d00329e023a7bdc71
Roaldys Garcia <64481181+roalfrank@users.noreply.github.com>
2023-07-02 15:39:01 -0400

    creado el create y list de post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BloglistView
+from .views import BloglistView,BlogCreateView
 
 app_name = 'blog'
 
 urlpatterns=[
-    path('', BloglistView.as_view(), name='home')
+    path('', BloglistView.as_view(), name='home'),
+    path('create/',BlogCreateView.as_view(),name='create')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
+from .models import Post
+from .forms import PostCreateForm
 
 
 class BloglistView(View):
     def get(self,request,*args, **kwargs):
         context = {
-
+           'lista_post': Post.objects.all()
         }
         return render(request, 'blog.html',context)
+    
+
+class BlogCreateView(View):
+    def get(self,request, *args,**kwargs):
+        form=PostCreateForm
+        context={
+           'form':form
+        }
+        return render(request, 'blog_create.html',context)
+    
+    def post(self,request, *args,**kwargs):
+
+        if request.method == 'POST':
+            form =  PostCreateForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+                form.save() 
+                return redirect('blog:home')
+
+        context={
+
+        }
+        return render(request, 'blog_create.html',context)
+    
